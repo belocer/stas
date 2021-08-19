e41e66fc7968be38df2dc050f07994b1748f669a
Denis Belocerkovec <belocerkovecden@gmail.com>
2021-08-19 15:09:36 +0300

    just in case

--- app/js/validation-form.js
+++ app/js/validation-form.js
@@ -9,6 +9,8 @@ window.addEventListener('load', () => {
     let label__email = document.querySelector('label[for="form__email"]');
     let label__phone = document.querySelector('label[for="form__phone"]');
 
+    let form__errors = document.querySelectorAll('.form__errors');
+
     let form__btn = document.querySelector('.form__btn');
 
     let arrErrors = [];
@@ -16,22 +18,38 @@ window.addEventListener('load', () => {
 // Регулярки для полей
     let regPhone = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/; // телефон
     let regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // Email
+    let regName = /^[a-zA-Zа-яА-Я]+$/ui; // Имя
+
+    let libErrorsText = {
+        phoneFormat: 'Указанный номер не соответствует формату!',
+        phoneEmpty: 'Не заполнено поле, - номера телефона!',
+        emailFormat: 'Указанный email не соответствует формату!',
+        emailEmpty: 'Не заполнено поле, - E-mail!',
+        nameEmpty: 'Не верно заполнено поле, - Имя!',
+    }
 
     header__form.querySelectorAll('.form__input').forEach(item => item.addEventListener('input', validateForm));
 
     function validateForm(e) {
         e.preventDefault()
-        if (!regexMatchCheck(regPhone, form__phone.value)) {
-            if (!arrErrors.find(item => item.place === 'form__phone')) {
-                arrErrors.push({place: 'form__phone', textError: 'Телефон - Не соответствует!'})
+        validateFormController(regPhone, form__phone, label__phone, libErrorsText.phoneFormat)
+        validateFormController(regEmail, form__email, label__email, libErrorsText.emailFormat)
+        validateFormController(regName, form__name, label__name, libErrorsText.nameEmpty)
+    }
+
+    function validateFormController(regularExpression, field, label, errorsText) {
+        if (!regexMatchCheck(regularExpression, field.value) && field.value.length > 2) {
+            if (!arrErrors.find(item => item.place === field.id)) {
+                arrErrors.push({place: field.id, textError: errorsText})
             }
-            showErrors(label__phone.querySelector('.form__errors'));
+            showErrors(label.querySelector('.form__errors'));
             switchBtn(false);
         } else {
             switchBtn(true);
-            deleteElArrErrors(form__phone)
-            showErrors(label__phone.querySelector('.form__errors'));
+            deleteElArrErrors(field)
+            showErrors(label.querySelector('.form__errors'));
         }
+        console.log(arrErrors);
     }
 
     function deleteElArrErrors(el) {
@@ -49,10 +67,11 @@ window.addEventListener('load', () => {
                 let li = document.createElement('LI');
                 li.textContent = item.textError;
                 ul.appendChild(li);
-                label.innerHTML = ul.innerHTML;
+                let error_place = document.querySelector('#' + item.place).parentNode;
+                error_place.querySelector('.form__errors').innerHTML = ul.innerHTML;
             })
         } else {
-            label.innerHTML = ''
+            form__errors.forEach(item => item.innerHTML = '')
         }
     }
 
